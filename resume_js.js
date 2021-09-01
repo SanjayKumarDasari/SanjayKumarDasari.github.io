3a0e744e179b5c52f76e293e778af4d3f227afc0
SanjayKumarDasari <sanju21101999@gmail.com>
2021-09-02 00:15:41 +0530

    menu sticky

--- resume_js.js
+++ resume_js.js
@@ -6,26 +6,24 @@ function openToggle(op){
 	var p = window.getComputedStyle(op);
 	var b = document.getElementById("main");
 	b.style.transition = "0.5s";
-	if(p.getPropertyValue("right") === "30px")
+	if(p.getPropertyValue("right") === "0px")
 	{
 		navbar.style.width = "250px";
-		op.style.right =  "200px";
+		op.style.right =  "10px";
 		nav.style.display = "block";
 		b.style.marginRight = "250px";
 	}
 	else {
 		navbar.style.width = "0px";
 		nav.style.display = "none";
-		op.style.right = "30px";
+		op.style.right = "0px";
 		b.style.marginRight = "0px";
 	}
 }
 
-function menuClicked(op , y)
+function menuClicked(op)
 {
 	var s = window.getComputedStyle(op);
-	var i;
-	
 	dis = s.getPropertyValue("display");
 	if(dis === "block")
 	{
@@ -38,4 +36,27 @@ function menuClicked(op , y)
 		tabs[i].style.display = "none";
 	}
 	y.style.display = "block"; */
+}
+var x = window.matchMedia("(max-width: 740px)");
+var y = window.matchMedia("(min-width: 740px)");
+x.addListener(setH);
+y.addListener(rsetH);
+function rsetH() {
+	var menu_v = document.getElementById("menu");
+				//alert(content_h);
+				if(y.matches){
+				menu_v.style.height = "100";
+				}
+}
+function setH(){
+				//alert("called");
+				
+				if(x.matches){
+				var content_v = document.getElementById("content");	
+				var content_h = content_v.clientHeight;
+				var menu_v = document.getElementById("menu");
+				//alert(content_h);
+				menu_v.style.height = content_h+"px";
+				//alert(menu_v.style.height);
+				}
 }
